f80e376c15681c565276d9aa688e1557d7ced1d1
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-09-07 22:24:16 +0300

    changes 2

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -3,12 +3,16 @@ package arrays;
 public class Main {
     static String[] names;
     static int count;
+
+    static String product;
     static int[] days;
 
     public static void main(String[] args) {
 
         names = new String[]{"Pen", "Tire", "Dualshok", "Paper",
                 "Water", "Kreker", "Nike", "Tire"};
+
+        product = "Nike";
 
         days = new int[]{1,2,3,4,5,6,7,8};
 
@@ -17,17 +21,15 @@ public class Main {
             System.out.printf("%d) %d week buy product %s %n",
                     count, days[i], names[i]);
 
-
         }
-        String product = "Tire";
+
         for (String name : names) {
 
             if (name.equals(product)) {
-                count++;
-                System.out.println(count + ") name is " + name);
+
+                System.out.println("Product is " + name);
             }
         }
-
 
         if (count > 0)
             System.out.println("Number of " + product +
@@ -35,6 +37,7 @@ public class Main {
 
         if (count == 0)
             System.out.println("No data :(");
+
 
     }
 
